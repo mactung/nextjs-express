4ef85bd83348aecfdc3895b30063c6391f9e1417
MacTung <mactrantung@gmail.com>
2020-12-05 22:14:01 +0700

    add body parser

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
+
+const bodyParser = require('body-parser');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -9,7 +11,8 @@ const handle = app.getRequestHandler();
 const apiRouter = require('./routes/api');
 
 app.prepare().then(() => {
-    const server = express()
+    const server = express();
+    server.use(bodyParser.json());
     
     server.use('/api', apiRouter);
 
